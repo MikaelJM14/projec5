c9625332682a282573540d07b39808e320b354ba
mikael <mikael.programing.c@gmail.com>
2024-07-04 21:02:18 +0100

    pls

--- Projec4/Projec4/Program.cs
+++ Projec4/Projec4/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using Projec4;
+
+namespace Projec4
+{
+    internal class StudentManangeSystemSMS
+    {
+        private static void Main(string[] args)
+        {
+        
+        }
+    }
+}
